84d1ae1979030a02a7b0f611e115ae5396ea63a2
Eclipse_K <godo8522@naver.com>
2023-01-31 16:31:04 +0900

    Add nav.js

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <NavLink>Home</NavLink>
+      </div>
+    </nav>
+  );
+};
