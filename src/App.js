87f89d11984dd656c60dcd7b2496ca671695b4b9
Eclipse_K <godo8522@naver.com>
2023-01-31 22:21:01 +0900

    Edit App.js connect Route, Switch, Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
-      <p>animation page</p>
-    </div>
-  );
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "./components/nav";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/category" component={Category} />
+        </Switch>
+        <Nav />
+      </div>
+    );
+  }
 }
 
 export default App;
