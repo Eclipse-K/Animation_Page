eb7b49e32802a04cefaf1cce8e8059e861f5007d
Eclipse_K <godo8522@naver.com>
2023-02-02 00:18:40 +0900

    Revert "Edit index and App"
    This reverts commit c0fe576d86e6a3b36559b6e1c37fc88de3e36201.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Nav from "./components/nav";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/category" component={Category} />
-        </Switch>
-        <Nav />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <p>Edit</p>
+    </div>
+  );
 }
 
 export default App;
