1c4812bcdf0ac7430560e177fe6cea71b924a043
Eclipse_K <godo8522@naver.com>
2023-02-06 16:09:54 +0900

    Edit index.js ReactDom

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <p>Edit</p>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>id</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
