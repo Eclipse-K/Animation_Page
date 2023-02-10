66ca62c6dfe2dfd542f53d13999fec76593259f4
Eclipse_K <godo8522@naver.com>
2023-02-10 19:16:22 +0900

    initial commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import AinNavbar from "./AniNavbar";
+import Motion from "./Motion";
 
 function App() {
   return (
     <div>
       <div>
         <AinNavbar />
+      </div>
+      <div>
+        <Motion />
       </div>
     </div>
   );
