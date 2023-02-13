8369f8734522a44a72f32a259ea658e69ae41136
Eclipse_K <godo8522@naver.com>
2023-02-13 21:46:25 +0900

    Edit Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <nav className="Navbar">
       <h1 className="navbar-logo">
-        React<i className="fab fa-react"></i>
+        Animation<i className="fab fa-react"></i>
       </h1>
       <div className="menu-icon" onClick={handleClick}>
         <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
