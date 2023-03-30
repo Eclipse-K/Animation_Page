25a2d325b7ff93e363996fa078b45e91c715ce14
Eclipse_K <godo8522@naver.com>
2023-03-30 14:55:43 +0900

    Edit list and card css

--- src/Components/Temporarily/AniItem.js
+++ src/Components/Temporarily/AniItem.js
@@ -4,7 +4,7 @@ const AniItem = ({ item }) => {
   const { title, year, since, content } = item;
   return (
     <li className="card">
-      <h2>연대: {since}</h2>
+      <h3>연대: {since}</h3>
       <h3>{title}</h3>
       <p>방영연도: {year}</p>
       <p>개요: {content}</p>
