33aea5705871761d6395c59dc5911ddfe714cd4e
Eclipse_K <godo8522@naver.com>
2023-05-01 21:11:36 +0900

    Add Copyright.css

--- src/Components/Copyright.js
+++ src/Components/Copyright.js
@@ -1,6 +1,8 @@
+import "./Copyright.css";
+
 function Copyright() {
   return (
-    <footer>
+    <footer className="under_sentence">
       <p>Copyright © 2023 My Company. All rights reserved.</p>
     </footer>
   );
