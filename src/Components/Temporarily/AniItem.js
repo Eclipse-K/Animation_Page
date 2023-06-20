46fa18f6cdfc943200851f3c8f142ee85e370102
Eclipse_K <godo8522@naver.com>
2023-06-20 14:57:53 +0900

    Edit dropmenu css

--- src/Components/Temporarily/AniItem.js
+++ src/Components/Temporarily/AniItem.js
@@ -4,10 +4,10 @@ const AniItem = ({ item }) => {
   const { title, year, since, content } = item;
   return (
     <li className="info_card">
-      <h3>연대: {since}</h3>
-      <h3>{title}</h3>
-      <p>방영연도: {year}</p>
-      <p>개요: {content}</p>
+      <h3>- 연 대: {since}</h3>
+      <h3>- {title}</h3>
+      <p>- 방영연도: {year}</p>
+      <p>- {content}</p>
     </li>
   );
 };
