0408695589b1ffac9d844591e879f4cdb7fb1cec
Eclipse_K <godo8522@naver.com>
2023-06-27 00:56:17 +0900

    Edit AniList font-family

--- src/Components/Temporarily/AniItem.js
+++ src/Components/Temporarily/AniItem.js
@@ -1,11 +1,10 @@
 import "./AniItem.css";
 
 const AniItem = ({ item }) => {
-  const { title, year, since, content } = item;
+  const { title, year, content } = item;
   return (
     <li className="info_card">
-      <h2>- 연 대: {since}</h2>
-      <h2>- {title}</h2>
+      <h1>{title}</h1>
       <h2>- 방영연도: {year}</h2>
       <p>- {content}</p>
     </li>
