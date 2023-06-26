148416f1cbff6a599a02cd162e9513548d8bc685
Eclipse_K <godo8522@naver.com>
2023-06-26 23:44:29 +0900

    Edit AniItme

--- src/Components/Temporarily/AniItem.js
+++ src/Components/Temporarily/AniItem.js
@@ -4,9 +4,9 @@ const AniItem = ({ item }) => {
   const { title, year, since, content } = item;
   return (
     <li className="info_card">
-      <h3>- 연 대: {since}</h3>
-      <h3>- {title}</h3>
-      <p>- 방영연도: {year}</p>
+      <h2>- 연 대: {since}</h2>
+      <h2>- {title}</h2>
+      <h2>- 방영연도: {year}</h2>
       <p>- {content}</p>
     </li>
   );
