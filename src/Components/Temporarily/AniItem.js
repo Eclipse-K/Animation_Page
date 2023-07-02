ccf9bcf38bc8fdeadb1e178de13730857fc9a944
Eclipse_K <godo8522@naver.com>
2023-07-02 15:34:33 +0900

    Edit AniItem overlay hover

--- src/Components/Temporarily/AniItem.js
+++ src/Components/Temporarily/AniItem.js
@@ -4,9 +4,11 @@ const AniItem = ({ item }) => {
   const { title, year, content } = item;
   return (
     <li className="info_card">
-      <h1>{title}</h1>
-      <h2>- 방영연도: {year}</h2>
-      <p>- {content}</p>
+      <sapn className="card_content">
+        <h1>{title}</h1>
+        <h2>- 방영연도: {year}</h2>
+        <p>- {content}</p>
+      </sapn>
     </li>
   );
 };
