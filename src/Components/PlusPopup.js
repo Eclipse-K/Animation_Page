e5140107a947709e85c6fb3d09f3c2a01f4fcd77
Eclipse_K <godo8522@naver.com>
2023-07-12 22:47:27 +0900

    Add PopupCard

--- src/Components/PlusPopup.js
+++ src/Components/PlusPopup.js
@@ -1,10 +1,12 @@
 import "./PlusPopup.css";
+import PopupCard from "./PopupCard";
 
 function PlusPopup({ onClose }) {
   return (
     <div className="plus-popup-container">
       <div className="plus-popup-content">
         <h1>추가하기</h1>
+        <PopupCard />
         <button className="close-button" onClick={onClose}>
           닫기
         </button>
