17521fc1b39f649d5beb45cb99418c97e097fa94
Eclipse_K <godo8522@naver.com>
2023-07-16 15:29:38 +0900

    Add PlusLabel

---
+++ src/Components/PlusOption/PlusLabel.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const PlusLabel = styled.label`
+  font-size: 15px;
+`;
+
+export default PlusLabel;
--- src/Components/PlusPopup.js
+++ src/Components/PlusPopup.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import PlusCloseButton from "./PlusOption/PulsCloseButton";
 import PlusInput from "./PlusOption/PlusInput";
+import PlusLabel from "./PlusOption/PlusLabel";
 
 const Container = styled.div`
   border: 1px solid black;
+  border-radius: 20px;
   position: fixed;
   top: 50%;
   left: 50%;
@@ -24,10 +26,14 @@ function PlusPopup({ onClose }) {
   return (
     <Container>
       <form>
-        <h1>추가하기</h1>
+        <h2>추가하기</h2>
+        <PlusLabel htmlFor="title">제목</PlusLabel>
         <PlusInput text="제목" id="title" name="title" placeholder="제목" />
+        <PlusLabel htmlFor="since">연대</PlusLabel>
         <PlusInput text="연대" id="since" name="since" placeholder="연대" />
+        <PlusLabel htmlFor="year">연도</PlusLabel>
         <PlusInput text="연도" id="year" name="year" placeholder="연도" />
+        <PlusLabel htmlFor="content">내용</PlusLabel>
         <PlusInput text="내용" id="content" name="content" placeholder="내용" />
         <PlusCloseButton onClick={onClose}>닫기</PlusCloseButton>
       </form>
