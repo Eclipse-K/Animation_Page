063e848879e6798dbf6b2f740817e76d14dc1aa5
Eclipse_K <godo8522@naver.com>
2023-07-20 14:58:40 +0900

    Add handleOutsideClick and Delete PlusCloseButton

--- src/Components/PlusPopup.js
+++ src/Components/PlusPopup.js
@@ -1,9 +1,25 @@
-import PlusCloseButton from "./PlusOption/PulsCloseButton";
 import PlusInput from "./PlusOption/PlusInput";
 import PlusLabel from "./PlusOption/PlusLabel";
 import PlusContainer from "./PlusOption/PlusContainer";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 function PlusPopup({ onClose }) {
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (popupRef.current && !popupRef.current.contains(e.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [onClose]);
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       e.preventDefault();
@@ -12,7 +28,7 @@ function PlusPopup({ onClose }) {
 
   return (
     <PlusContainer onKeyDown={handleKeyDown} tabIndex="0">
-      <form>
+      <form ref={popupRef}>
         <h2>추가하기</h2>
         <PlusLabel htmlFor="title">제목</PlusLabel>
         <PlusInput text="제목" id="title" name="title" placeholder="제목" />
@@ -22,7 +38,6 @@ function PlusPopup({ onClose }) {
         <PlusInput text="연도" id="year" name="year" placeholder="연도" />
         <PlusLabel htmlFor="content">내용</PlusLabel>
         <PlusInput text="내용" id="content" name="content" placeholder="내용" />
-        <PlusCloseButton onClick={onClose}>닫기</PlusCloseButton>
       </form>
     </PlusContainer>
   );
