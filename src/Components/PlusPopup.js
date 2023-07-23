29a8d9702281d9c48893ebe74b89a5f6d8c2db90
Eclipse_K <godo8522@naver.com>
2023-07-23 15:54:12 +0900

    Edit handleConfirmClick

--- src/Components/PlusPopup.js
+++ src/Components/PlusPopup.js
@@ -4,6 +4,7 @@ import PlusInsertButton from "./PlusOption/PulsInsertButton";
 import PlusContainer from "./PlusOption/PlusContainer";
 import { useRef } from "react";
 import { useEffect } from "react";
+//import axios from "axios";
 
 function PlusPopup({ onClose }) {
   const popupRef = useRef(null);
@@ -27,6 +28,34 @@ function PlusPopup({ onClose }) {
     }
   };
 
+  const handleConfirmClick = () => {
+    // "확인" 버튼을 클릭했을 때의 동작을 처리하기 위한 로직을 여기에 추가합니다.
+    const titleInput = document.getElementById("title");
+    const sicneInput = document.getElementById("since");
+    const yearInput = document.getElementById("year");
+    const contentInput = document.getElementById("content");
+
+    // eslint-disable-next-line
+    const newItem = {
+      title: titleInput.value,
+      since: sicneInput.value,
+      year: yearInput.value,
+      content: contentInput.value,
+    };
+
+    /* Axios를 사용하여 API 요청을 보내는 부분입니다.
+    axios
+      .post("https://example-api.com/items", newItem)
+      .then((response) => {
+        console.log("새로운 아이템이 추가되었습니다!", response.data);
+        onClose(); // API 요청이 성공하면 팝업을 닫습니다.
+      })
+      .catch((error) => {
+        console.error("아이템 추가에 실패했습니다:", error);
+      });
+      */
+  };
+
   return (
     <PlusContainer onKeyDown={handleKeyDown} tabIndex="0">
       <form ref={popupRef}>
@@ -39,7 +68,7 @@ function PlusPopup({ onClose }) {
         <PlusInput text="연도" id="year" name="year" placeholder="연도" />
         <PlusLabel htmlFor="content">내용</PlusLabel>
         <PlusInput text="내용" id="content" name="content" placeholder="내용" />
-        <PlusInsertButton>확인</PlusInsertButton>
+        <PlusInsertButton onClick={handleConfirmClick}>확인</PlusInsertButton>
       </form>
     </PlusContainer>
   );
