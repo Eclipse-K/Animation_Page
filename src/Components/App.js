db43a443b525ef2a5613231c52ee3a37c7a550e9
Eclipse_K <godo8522@naver.com>
2023-08-22 22:15:18 +0900

    Edit App and Add Home

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,28 @@
-import NavDrop from "./NavDrop";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavDrop from "./NavDrop";
 import Search from "./SearchBar";
 import Copyright from "./Copyright";
-import { Route, Routes } from "react-router-dom";
 import Since1970 from "./Since/Since1970";
 import Since1980 from "./Since/Since1980";
 import Since1990 from "./Since/Since1990";
 import Since2000 from "./Since/Since2000";
 
+function Home() {
+  return (
+    <div>
+      <NavDrop />
+      <Search />
+      <Copyright />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App-container">
-      <div>
-        <NavDrop />
-      </div>
-
-      <Search />
-
-      <Copyright />
-
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/Since1970" element={<Since1970 />} />
         <Route path="/Since1980" element={<Since1980 />} />
         <Route path="/Since1990" element={<Since1990 />} />
