8320f37a6039b52d2c47536b92e3dd91de419158
Eclipse_K <godo8522@naver.com>
2023-11-13 21:44:04 +0900

    Add Home.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import NavDrop from "./NavDrop";
-import Search from "./SearchBar";
-import Copyright from "./Copyright";
 import Since1970 from "./Since/Since1970";
 import Since1980 from "./Since/Since1980";
 import Since1990 from "./Since/Since1990";
 import Since2000 from "./Since/Since2000";
-
-function Home() {
-  return (
-    <div>
-      <NavDrop />
-      <Search />
-      <Copyright />
-    </div>
-  );
-}
+import Home from "./Home";
 
 function App() {
   return (
---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import NavDrop from "./NavDrop";
+import Search from "./SearchBar";
+import Copyright from "./Copyright";
+
+function Home() {
+  return (
+    <div>
+      <NavDrop />
+      <Search />
+      <Copyright />
+    </div>
+  );
+}
+
+export default Home;
