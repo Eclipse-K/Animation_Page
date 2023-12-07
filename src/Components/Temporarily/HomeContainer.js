bbd1c7d8f42d2f78a8178293b9dc36fce7e937f2
Eclipse_K <godo8522@naver.com>
2023-12-07 19:57:57 +0900

    Edit info container List type

--- src/Components/Temporarily/HomeContainer.js
+++ src/Components/Temporarily/HomeContainer.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 const HomeContainer = styled.div`
+  display: flex;
   height: 855px;
-  position: relative;
+  align-items: flex-end;
 
   @media all and (max-width: 432px) {
     height: 930px;
