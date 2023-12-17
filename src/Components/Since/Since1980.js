1fb14fa1a4d375bab66b46edb45da41fb370bd54
Eclipse_K <godo8522@naver.com>
2023-12-17 15:22:28 +0900

    Edit Since1980 ~ 2000

--- src/Components/Since/Since1980.js
+++ src/Components/Since/Since1980.js
@@ -19,14 +19,16 @@ function Since1980() {
   const [isPlusPopup, setIsPlusPopup] = useState(false);
   const [showLoadMore, setShowLoadMore] = useState(true); // 추가할 데이터가 더 있는지 여부를 나타내는 상태 추가
   const [showOnlyOneItem, setShowOnlyOneItem] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(10); //현재 표시되는 데이터 개수
 
   const handleShowMore = () => {
-    const newVisibleCount = data.length + 1;
+    const newVisibleCount = visibleCount + 5;
     const filterData = anijson.filter((item) => item.since === "1980");
     const sortedData = filterData.sort((a, b) => a[order] - b[order]);
     const newData = sortedData.slice(0, newVisibleCount);
     if (newData.length > data.length) {
       setData(newData);
+      setVisibleCount(newVisibleCount);
     } else {
       // 더 이상 추가할 데이터가 없으면 load-more 버튼을 숨김
       setShowLoadMore(false);
@@ -35,15 +37,20 @@ function Since1980() {
 
   const handleToggleVisibility = () => {
     setShowOnlyOneItem(!showOnlyOneItem);
+    if (showOnlyOneItem) {
+      setShowLoadMore(true); // 전체보기 모드로 전환 시 더보기 버튼 다시 활성화
+    }
   };
 
   // visibleCount가 변경될 때마다 데이터 업데이트
   useEffect(() => {
     const filterData = anijson.filter((item) => item.since === "1980");
     const sortedData = filterData.sort((a, b) => a[order] - b[order]);
-    const newData = showOnlyOneItem ? sortedData.slice(0, 1) : sortedData;
+    const newData = showOnlyOneItem
+      ? sortedData.slice(0, visibleCount)
+      : sortedData;
     setData(newData);
-  }, [showOnlyOneItem, order]);
+  }, [showOnlyOneItem, order, visibleCount]);
 
   //추가하기 기능
   const handleAddClick = () => {
@@ -75,13 +82,13 @@ function Since1980() {
           )}
 
           {showOnlyOneItem ? (
-            <BsCardList
+            <BsCollectionPlayFill
               className="Full-view"
               onClick={handleToggleVisibility}
               alt="전체보기"
             />
           ) : (
-            <BsCollectionPlayFill
+            <BsCardList
               className="View-only-one"
               onClick={handleToggleVisibility}
               alt="1개만 보기"
