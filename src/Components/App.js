920cd2747b701d2dfb12fa8552dee439a78b84d2
Eclipse_K <godo8522@naver.com>
2023-12-26 14:53:35 +0900

    Move right and dark mode in Home

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,34 +5,19 @@ import Since1980 from "./Since/Since1980";
 import Since1990 from "./Since/Since1990";
 import Since2000 from "./Since/Since2000";
 import Home from "./Home";
-import Copyright from "./Copyright";
 import SearchBar from "./SearchBar";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [modeChange, setModeChange] = useState(false);
-  //시스템 설정에 따라 테마 색상이 변함.
-
-  useEffect(() => {
-    const prefersModeChange = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    setModeChange(prefersModeChange);
-  }, []);
-
   return (
     <div className="App-container">
-      <div className={modeChange ? "dark-mode" : "light-mode"}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Since1970" element={<Since1970 />} />
-          <Route path="/Since1980" element={<Since1980 />} />
-          <Route path="/Since1990" element={<Since1990 />} />
-          <Route path="/Since2000" element={<Since2000 />} />
-          <Route path="/SearchBar" element={<SearchBar />} />
-        </Routes>
-      </div>
-      <Copyright />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Since1970" element={<Since1970 />} />
+        <Route path="/Since1980" element={<Since1980 />} />
+        <Route path="/Since1990" element={<Since1990 />} />
+        <Route path="/Since2000" element={<Since2000 />} />
+        <Route path="/SearchBar" element={<SearchBar />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,14 +2,30 @@ import NavDrop from "./NavDrop";
 import Minesweeper from "./Minesweeper";
 import HomeContainer from "./Temporarily/HomeContainer";
 import Title from "./Temporarily/Title";
+import "./Home.css";
+import { useEffect, useState } from "react";
+import Copyright from "./Copyright";
 
 function Home() {
+  const [modeChange, setModeChange] = useState(false);
+  //시스템 설정에 따라 테마 색상이 변함.
+
+  useEffect(() => {
+    const prefersModeChange = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setModeChange(prefersModeChange);
+  }, []);
+
   return (
-    <HomeContainer>
-      <NavDrop />
-      <Title to="/">Animation</Title>
-      <Minesweeper />
-    </HomeContainer>
+    <div className={modeChange ? "dark-mode" : "light-mode"}>
+      <HomeContainer>
+        <NavDrop />
+        <Title to="/">Animation</Title>
+        <Minesweeper />
+        <Copyright />
+      </HomeContainer>
+    </div>
   );
 }
 
