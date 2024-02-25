350a466a4ed65cdc54d4853c2d55a68b9c0c3418
Eclipse_K <godo8522@naver.com>
2024-02-25 23:00:40 +0900

    Edit HomeGrid width:280px

--- src/Components/HomeStyle/HomeGrid.js
+++ src/Components/HomeStyle/HomeGrid.js
@@ -8,7 +8,13 @@ const HomeGrid = styled.div`
   top: -10%;
   border: 1px solid black;
   grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+
+  @media all and (width: 280px) {
+    top: 2%;
+    height: 450px;
+    grid-template-columns: 1fr;
+  }
 
   @media all and (min-width: 281px) and (max-width: 432px) {
     top: 0;
