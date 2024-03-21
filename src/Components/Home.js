5c4c633e12185511442141c6912dad5cf3102ede
Eclipse_K <godo8522@naver.com>
2024-03-21 14:56:16 +0900

    Edit Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,10 @@ import HomeContainer from "./HomeStyle/HomeContainer.js";
 import Title from "./Temporarily/Title";
 import Copyright from "./Copyright";
 import HomeGrid from "./HomeStyle/HomeGrid.js";
-import HomeSecond from "./HomeStyle/HomeSecond.js";
-import MickeyMouse from "../image/MickeyMouse.JPG";
-import Mazinger from "../image/마징가.png";
-import HomeThird from "./HomeStyle/HomeThird.js";
+// import HomeSecond from "./HomeStyle/HomeSecond.js";
+// import MickeyMouse from "../image/MickeyMouse.JPG";
+// import Mazinger from "../image/마징가.png";
+// import HomeThird from "./HomeStyle/HomeThird.js";
 
 function Home() {
   return (
@@ -16,8 +16,8 @@ function Home() {
       <Title>Animation</Title>
       <HomeGrid>
         <Minesweeper />
-        <HomeSecond src={MickeyMouse} alt="MickeyMouse" />
-        <HomeThird src={Mazinger} alt="Mazinger" />
+        {/* <HomeSecond src={MickeyMouse} alt="MickeyMouse" />
+        <HomeThird src={Mazinger} alt="Mazinger" /> */}
       </HomeGrid>
 
       <Copyright />
