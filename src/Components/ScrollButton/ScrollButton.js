27904c1f93fa91cad21dba26311ec3e0b7929206
Eclipse_K <godo8522@naver.com>
2024-04-02 14:00:54 +0900

    Edit scrollbutton

--- src/Components/ScrollButton/ScrollButton.js
+++ src/Components/ScrollButton/ScrollButton.js
@@ -1,10 +1,11 @@
 import ScrollStyle from "./ScrollStyle";
 
-function ScrollButton({ scrollRef }) {
+function ScrollButton() {
   const scrollToTop = () => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTo({ top: 0, behavior: "smooth" });
-    }
+    // if (scrollRef.current) {
+    //   scrollRef.current.scrollTo({ top: 0, behavior: "smooth" });
+    // }
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return <ScrollStyle onClick={scrollToTop} />;
